70ab0db82a35273501b84f384cda46094c56af02
mojiez <937691466@qq.com>
2024-07-23 16:05:25 +0800

    完成图表管理页面 mychart，显示创建过的图表+根据name搜索 学习mybatis的底层原理

--- src/pages/MyChart/index.tsx
+++ src/pages/MyChart/index.tsx
@@ -1,162 +1,117 @@
-import { genChartByAiUsingPost } from '@/services/yibi/chartController';
-import { UploadOutlined } from '@ant-design/icons';
-import {
-  Button,
-  Card,
-  Col,
-  Divider,
-  Form,
-  Input,
-  message,
-  Row,
-  Select,
-  Space,
-  Spin,
-  Upload,
-} from 'antd';
+import { listMyChartVoByPageUsingPost } from '@/services/yibi/chartController';
+import { useModel } from '@umijs/max';
+import { Avatar, Card, List, message } from 'antd';
+import Search from 'antd/es/input/Search';
 import ReactECharts from 'echarts-for-react';
-import React, { useState } from 'react';
-
-const formItemLayout = {
-  labelCol: { span: 6 },
-  wrapperCol: { span: 14 },
-};
+import React, { useEffect, useState } from 'react';
 
 // const onFinish = (values: any) => {
 //   console.log('Received values of form: ', values);
 // };
 
-const AddChart: React.FC = () => {
-  const [option, setOption] = useState<any>();
-  const [chart, setChart] = useState<any>();
-  const [submitting, setSubmitting] = useState<boolean>(false);
-  const onFinish = async (values: any) => {
-    console.log('Received values of form: ', values);
-    if (submitting) {
-      // 如果说正在加载 直接返回 避免重复请求
-      return;
-    }
-    // 如果之前 没有加载 现在点击了提交 那就要设置为正在加载
-    setSubmitting(true);
-    setChart(undefined);
-    setOption(undefined);
-    const params = {
-      ...values,
-      file: undefined,
-    };
-    // params: API.genChartByAIUsingPOSTParams,
-    // body: {},
-    // file?: File,
-    // options?: { [key: string]: any },
+const MyChart: React.FC = () => {
+  // 获取原始数据
+  const initSearchParams = {
+    // 初始情况下返回每页12条数据
+    pageSize: 4,
+    current: 1,
+  };
+  // 定义发送给后端的查询条件
+  const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({ ...initSearchParams });
+  const [chartList, setChartList] = useState<API.Chart[]>([]);
+  const [total, setTotal] = useState<number>(0);
+  const [loading, setloading] = useState<boolean>(false);
+  // 从全局状态中获取到当前登陆的用户信息
+  const { initialState } = useModel('@@initialState');
+  const { currentUser } = initialState ?? {};
+  // 定义一个获取数据的异步函数
+  const loadData = async () => {
+    //调用后端的接口
+    setloading(true);
     try {
-      const res = await genChartByAiUsingPost(params, {}, values.file.file.originFileObj);
-      if (!res?.data) {
-        message.error('分析失败,没有取得数据');
+      const res = await listMyChartVoByPageUsingPost(searchParams);
+      if (res.data) {
+        // records里面是分页
+        setChartList(res.data.records ?? []);
+        // 获取数据的总条数
+        setTotal(res.data.total ?? 0);
       } else {
-        message.success('分析成功');
-        setChart(res.data);
-        // 解析成对象
-        const chartOption = JSON.parse(res.data.genChart ?? '');
-        if (!chartOption) {
-          throw new Error('图表代码解析错误');
-        } else {
-          setOption(chartOption);
-        }
+        // data为空 返回的数据为空
+        message.error('data为空');
+        throw new Error('data为空');
       }
-    } catch (e: any) {
-      message.error('分析失败，调用鱼聪明错误', e.message);
+    } catch (e) {
+      message.error('获取Chart失败', e.message);
     }
-    // 请求完以后 加载完成
-    setSubmitting(false);
+    setloading(false);
   };
 
+  // 首次页面加载时，触发加载数据,当searchParams改变时，也触发加载数据
+  useEffect(() => {
+    loadData();
+  }, [searchParams]);
   return (
-    // 多行输入 下拉输入 文件上传
-    <>
-      <div>
-        <h1>My Chart</h1>
+    <div className="my-chart">
+      <div className="margin-16">
+        <Search
+          placeholder="请输入图表名称"
+          enterButton
+          loading={loading}
+          onSearch={(value) => {
+            // 设置搜索条件
+            setSearchParams({
+              // 搜索条件
+              ...searchParams,
+              // 搜索词
+              name: value,
+            });
+          }}
+        />
       </div>
-      <div className="add-Chart">
-        <Row gutter={24}>
-          <Col span={12}>
-            <Form
-              name="validate_other"
-              {...formItemLayout}
-              onFinish={onFinish}
-              initialValues={{
-                'input-number': 3,
-                'checkbox-group': ['A', 'B'],
-                rate: 3.5,
-                'color-picker': null,
-              }}
-              style={{ maxWidth: 600 }}
-            >
-              <Form.Item
-                name="goal"
-                label="分析目标"
-                rules={[{ required: true, message: '请输入你的分析需求' }]}
-              >
-                <Input.TextArea showCount maxLength={100} />
-              </Form.Item>
-              <Form.Item name="name" label="图表名称">
-                <Input placeholder="请输入图表名称" />
-              </Form.Item>
-
-              <Form.Item
-                name="chartType"
-                label="图表类型"
-                hasFeedback
-                rules={[{ required: true, message: '请选择图表类型' }]}
-              >
-                <Select placeholder="请选择图表类型">
-                  <Option value="折线图">折线图</Option>
-                  <Option value="柱状图 ">柱状图</Option>
-                  <Option value="堆叠图 ">堆叠图</Option>
-                  <Option value="饼图 ">饼图</Option>
-                  <Option value="雷达图 ">雷达图</Option>
-                </Select>
-              </Form.Item>
-
-              <Form.Item name="file" label="原始数据">
-                <Upload name="file">
-                  <Button icon={<UploadOutlined />}>上传CSV文件</Button>
-                </Upload>
-              </Form.Item>
-
-              <Form.Item wrapperCol={{ span: 12, offset: 6 }}>
-                <Space>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    loading={submitting}
-                    disabled={submitting}
-                  >
-                    智能分析
-                  </Button>
-                  <Button htmlType="reset" loading={submitting} disabled={submitting}>
-                    重置
-                  </Button>
-                </Space>
-              </Form.Item>
-            </Form>
-          </Col>
-          <Col span={12}>
-            <Card title="分析结论">
-              {option ? <p>{chart?.genResult}</p> : <p>请先在左侧进行提交</p>}
-              <Spin spinning={submitting} />
+      <List
+        size="large"
+        grid={{
+          gutter: 16,
+          xs: 1,
+          sm: 1,
+          md: 1,
+          lg: 2,
+          xl: 2,
+          xxl: 2,
+        }}
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+            setSearchParams({
+              ...searchParams,
+              current: page,
+            });
+          },
+          pageSize: searchParams.pageSize,
+          total: total,
+        }}
+        loading={loading}
+        dataSource={chartList}
+        footer={
+          <div>
+            <b>yibi</b> my-chart
+          </div>
+        }
+        renderItem={(item) => (
+          <List.Item key={item.id}>
+            <Card>
+              <List.Item.Meta
+                avatar={<Avatar src={currentUser && currentUser.userAvatar} />}
+                title={item.name}
+                description={item.genResult}
+              />
+              {item.chartType}
+              {<ReactECharts option={JSON.parse(item.genChart)} />}
             </Card>
-            <Divider />
-            <Card title="生成图表">
-              {option ? <ReactECharts option={option} /> : <p>请先在左侧进行提交</p>}
-              <Spin spinning={submitting} />
-            </Card>
-          </Col>
-        </Row>
-
-        {/*生成图表：*/}
-        {/*{option && <ReactECharts option={option} />}*/}
-      </div>
-    </>
+          </List.Item>
+        )}
+      />
+    </div>
   );
 };
-export default AddChart;
+export default MyChart;
